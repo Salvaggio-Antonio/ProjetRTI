c7c12bf2aa26ec4fe3d1be3e578f04e5a6649f73
Antonio Salvaggio <antonio.salvaggio@student.hepl.be>
2022-12-13 12:06:21 +0100

    zap2

--- JavaServeur/src/Serveurs/FenAppServeur_Reservations.java
+++ JavaServeur/src/Serveurs/FenAppServeur_Reservations.java
@@ -28,8 +28,7 @@ public class FenAppServeur_Reservations extends javax.swing.JFrame implements Co
 
     private int port;
     
-    public String path = "Config/Config.config";
-    //public String path = "C:\\Users\\Salva\\Documents\\ecole\\important\\3emeinformatique\\rti\\Inpres-Enjoy your holidays\\JavaServeur\\src\\Config\\Config.config";
+    public String path = "C:\\Users\\Salva\\Documents\\ecole\\important\\3emeinformatique\\rti\\Inpres-Enjoy your holidays\\JavaServeur\\src\\Config\\Config.config";
 
     public FenAppServeur_Reservations() throws IOException {
         initComponents();
